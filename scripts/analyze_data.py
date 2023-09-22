4cf8364421a975bdd0ca9683607ad451d01ea0f9
VivaldoGP <VivaldoGP27@hotmail.com>
2023-09-22 00:06:38 -0600

    feat: NDVI stats

--- scripts/analyze_data.py
+++ scripts/analyze_data.py
@@ -1,20 +1,25 @@
 import rasterio
 import rasterstats
 
-import geopandas as gpd
 import pandas as pd
 
-from os import listdir
-from os import path
+import os
 
-# from datetime import datetime
-from datetime import date
+import datetime
 
-parcelas = gpd.read_file(r'C:\Users\DELL\PycharmProjects\Tesis\Poligonos\SHP\Rancho_SantaAnita'
-                         r'\Lote_Rancho_Santa_Anita.shp',
-                         encoding='utf-8')
+sen_images_path = r"C:\Users\DELL\PycharmProjects\Tesis\Tesis_caña"
+parcelas_path = r"C:\Users\DELL\PycharmProjects\Tesis\Poligonos\SHP\Rancho_SantaAnita\Lote_Rancho_Santa_Anita.shp"
 
-parcelas_wgs_84 = parcelas.to_crs(4326)
+
+def date_from_filename(filename: str):
+    try:
+        date_part = filename.split('_')[4]
+        year: int = int(date_part[:4])
+        month: int = int(date_part[4:6])
+        day: int = int(date_part[6:8])
+        return datetime.date(year=year, month=month, day=day)
+    except (TypeError, ValueError):
+        return None
 
 
 def ndvi(image):
@@ -29,50 +34,44 @@ def ndvi(image):
     return ndvi_calc
 
 
-sen_images_path = r"C:\Users\DELL\PycharmProjects\Tesis\imagenes_sentinel"
+def calc_ndvi(image_path: str, shp_path: str):
+    try:
+        with rasterio.open(image_path) as src:
+            transform = src.transform
+            ndvi_band = ndvi(src)
+
+            zonal_stats_ = rasterstats.zonal_stats(shp_path, ndvi_band, affine=transform,
+                                                   stats=['mean', 'max', 'min'])
+            return zonal_stats_
+    except (rasterio.errors.RasterioIOError, ValueError):
+        return None
+
 
 sen_images = {}
 
-try:
-    for file in listdir(sen_images_path):
-        if file.endswith('.tif'):
-            full_path = path.join(sen_images_path, file)
-            splited_file_name = file.split('_')
-            year, month, day = (int(splited_file_name[4][0:4]), int(splited_file_name[4][4:6]),
-                                int(splited_file_name[4][6:8]))
-            fecha = date(year, month, day)
+for file in os.listdir(sen_images_path):
+    if file.endswith('.tif'):
+        full_path = os.path.join(sen_images_path, file)
+        fecha = date_from_filename(filename=file)
+        if fecha:
             sen_images[fecha] = full_path
-except ValueError:
-    print('algo salio mal')
 
-stats_ndvi = {}
+ndvi_stats = {}
 
-try:
-    for fecha, image_path in sen_images.items():
-
-        imagen = image_path.split('_')
-        src = rasterio.open(image_path)
-        transform = src.transform
-        ndvi_band = ndvi(src)
-
-        for num, feature in enumerate(rasterstats.zonal_stats(r'C:\Users\DELL\PycharmProjects\Tesis\Poligonos\SHP'
-                                                              r'\Rancho_SantaAnita\Lote_Rancho_Santa_Anita.shp',
-                                                              ndvi_band, affine=transform, stats=['mean', 'max',
-                                                                                                  'min'])):
-            # print(num, feature)
-
+for fecha, image_path_ in sen_images.items():
+    zonal_stats = calc_ndvi(image_path=image_path_, shp_path=parcelas_path)
+    if zonal_stats:
+        imagen = image_path_.split('_')
+        for num, feature in enumerate(zonal_stats):
             key = f"{num + 1}_{imagen[-3]}"
-            stats_ndvi[key] = {
-                'Imagen': imagen[-3],
-                'Fecha': fecha,
-                'Num_parcela': num + 1,
+            ndvi_stats[key] = {
+                'imagen': imagen[-3],
+                'fecha': fecha,
+                'num_parcela': num + 1,
                 'mean_ndvi': feature['mean'],
                 'max_ndvi': feature['max'],
                 'min_ndvi': feature['min']
             }
-    ndvi_df = pd.DataFrame.from_dict(stats_ndvi, orient='index')
-    ndvi_df.reset_index(inplace=True)
-except TypeError:
-    print('chin')
 
-print(ndvi_df.loc[ndvi_df['Num_parcela'] == 1])
+ndvi_df = pd.DataFrame.from_dict(ndvi_stats, orient='index')
+ndvi_df.reset_index(inplace=True)
