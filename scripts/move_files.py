5f3f8628bb13fd4d9be386f8ee7f85221055f1d5
VivaldoGP <VivaldoGP27@hotmail.com>
2023-10-09 18:35:59 -0600

    feat: adapted to new images

--- scripts/move_files.py
+++ scripts/move_files.py
@@ -1,23 +1,20 @@
 import os
 import shutil
 
-main_path = r'C:\Users\DELL\PycharmProjects\Tesis\imagenes_2'
+main_path = r'G:\Mi unidad\Tesis_5'
 
-parcelas_id = [i.split('_')[1] for i in os.listdir(main_path) if i.endswith('.tif')]
-# print(set(parcelas_id))
+parcel_ids = [i.split('_')[0] for i in os.listdir(main_path) if i.endswith('.tif')]
 
-for i in parcelas_id:
+for id_ in set(parcel_ids):
     try:
-        os.makedirs(f"{main_path}\Parcela_{i}")
+        os.makedirs(f"{main_path}\Parcela_{id_}")
     except OSError:
-        os.rmdir(f"{main_path}\Parcela_{i}")
-        os.makedirs(f"{main_path}\Parcela_{i}")
+        os.rmdir(f"{main_path}\Parcela_{id_}")
+        os.makedirs(f"{main_path}\Parcela_{id_}")
 
-for root, dir_, file in os.walk(main_path):
-    print(root)
-    for j in file:
-        parcela_id = f"{j.split('_')[0]}_{j.split('_')[1]}"
-        for k in dir_:
-            if parcela_id == k:
-                # print(j, k)
-                shutil.move(os.path.join(main_path, j), f"{main_path}\{k}\{j}")
+for root, dirs, files in os.walk(main_path):
+    for file in files:
+        parcel_id = file.split('_')[0]
+        for dir_ in dirs:
+            if parcel_id == dir_.split('_')[1]:
+                shutil.move(os.path.join(main_path, file), f"{main_path}\{dir_}\{file}")
